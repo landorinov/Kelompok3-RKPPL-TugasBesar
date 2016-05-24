27bdc04192892156876338a8d2702aa13e1bc0c2
Firman Kusnandi <firman.kusnandi@gmail.com>
2016-05-24 23:35:01 +0700

    Update C.java

--- src/com/kelompok4/C.java
+++ src/com/kelompok4/C.java
@@ -2,4 +2,24 @@ package com.kelompok4;
 
 public class C {
   private double n1;
+  private double n2;
+  
+  public C(){
+  }
+  
+  public int add(){
+    
+  }
+  
+  public int sub(){
+    
+  }
+  
+  public double mul(){
+    
+  }
+  
+  public double div(){
+    
+  }
 }
