145b631ec5f511ce4ac2f5d411a740d0732d9c47
ilhamoperanda <ilhamoperanda@gmail.com>
2016-05-26 03:01:51 +0700

    penambahan function A dan return toString

--- src/com/kelompok4/A.java
+++ src/com/kelompok4/A.java
@@ -1,5 +1,17 @@
 package com.kelompok4;
 
 public class A {
+	// function a
+	public A() {
+		
+	}
 
+	// mengembalikan nilai string
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+	
 }
