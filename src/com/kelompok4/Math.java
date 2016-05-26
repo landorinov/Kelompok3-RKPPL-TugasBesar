94004ea7152e9333b74aefeb65c096f1d13d615b
Faerul Salamun <faerulsalamun@gmail.com>
2016-05-26 21:58:25 +0700

    Add Math function method mod

--- src/com/kelompok4/Math.java
+++ src/com/kelompok4/Math.java
@@ -1,9 +1,11 @@
 package com.kelompok4;
 
+import static java.lang.Math.sqrt;
+
 public class Math {
 
     public int mod(int n1, int n2) {
-        return 0;
+        return n1 % n2;
     }
 
     public double sqrt(double n) {
