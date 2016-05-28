4c4b2bd6672ccae318c9851384abbbeafa6b4bc1
Otik <otik.133040040@mail.unpas.ac.id>
2016-05-28 08:51:18 +0700

    TestB (testNG)

---
+++ src/com/Test/TestB.java
@@ -0,0 +1,22 @@
+package com.Test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.kelompok4.B;
+
+public class TestB {
+	B b = new B();
+	
+	@Test
+	//Mengecek apakah hasil dari mul yg di ekspektasikan tidak sesuai dengan yang ditetapkan
+	public void test1(){
+		Assert.assertNotEquals(b.mul(2,5), 5);
+	}
+	
+	@Test
+	//Mengecek apakah hasil dari mul yg di ekspektasikan sesuai dengan yang ditetapkan
+	public void test2(){
+		Assert.assertEquals(b.mul(2,5), 10.0);
+	}
+}
